50a3af6200a2e268aa83a8474560c3a8f87de6e9
Anthomate <anthony.sapet@outlook.com>
2023-04-20 09:14:15 +0200

    end

--- api.ts
+++ api.ts
@@ -1,19 +1,27 @@
-import { Buffer } from "buffer";
-import * as FileSystem from "expo-file-system";
+import { Buffer } from 'buffer';
+import * as FileSystem from 'expo-file-system';
 
-export const readConfig = async () => {
+export interface Config {
+    username: string;
+    password: string;
+    subdomain: string;
+    topLevelDomain?: string;
+    protocol?: string;
+}
+
+export const readConfig = async (): Promise<Config | null> => {
     try {
         const configPath = `${FileSystem.documentDirectory}config.json`;
         const configJson = await FileSystem.readAsStringAsync(configPath, {
             encoding: FileSystem.EncodingType.UTF8,
         });
-        const config = JSON.parse(configJson);
+        const config = JSON.parse(configJson) as Config;
         return {
             username: config.username,
             password: config.password,
             subdomain: config.subdomain,
-            topLevelDomain: config?.topLevelDomain || "fr",
-            protocol: config?.protocol || "http"
+            topLevelDomain: config?.topLevelDomain || 'fr',
+            protocol: config?.protocol || 'http',
         };
     } catch (error) {
         console.error(error);
@@ -21,7 +29,17 @@ export const readConfig = async () => {
     }
 };
 
-const fetchOpenTickets = async (dateMin, page, headers, apiUrl) => {
+interface Ticket {
+    StateName: string;
+    RequestDate: string;
+}
+
+const fetchOpenTickets = async (
+    dateMin: string,
+    page: number,
+    headers: Headers,
+    apiUrl: string
+): Promise<Ticket[]> => {
     const response = await fetch(`${apiUrl}/GetOpenTickets?p=${page}&datemin=${dateMin}`, { headers });
     if (!response.ok) {
         throw new Error(`API request failed with status ${response.status}`);
@@ -29,23 +47,32 @@ const fetchOpenTickets = async (dateMin, page, headers, apiUrl) => {
     return await response.json();
 };
 
-export const getOpenTickets = async (dateMin: string, username: any, password: any, subdomain: any, protocol: string, topLevelDomain: string, page = 1) => {
-
+export const getOpenTickets = async (
+    dateMin: string,
+    username: string,
+    password: string,
+    subdomain: string,
+    protocol: string,
+    topLevelDomain: string,
+    page = 1
+): Promise<Ticket[]> => {
     const API_BASE_URL = `${protocol}://${subdomain}-api.simplydesk.${topLevelDomain}/incidentManagement.svc`;
 
-    const token = Buffer.from(`${username}:${password}`).toString("base64");
+    const token = Buffer.from(`${username}:${password}`).toString('base64');
     const headers = new Headers();
-    headers.set("Authorization", `Basic ${token}`);
+    headers.set('Authorization', `Basic ${token}`);
 
     const tickets = await fetchOpenTickets(dateMin, page, headers, API_BASE_URL);
     if (tickets.length === 30) {
-        const nextPageTickets = await getOpenTickets(dateMin, username, password, subdomain, page + 1);
+        const nextPageTickets = await getOpenTickets(dateMin, username, password, subdomain, protocol, topLevelDomain, page + 1);
         return tickets.concat(nextPageTickets);
     }
     return tickets;
 };
 
-export const countTicketsByState = (tickets, stateName) =>
+export const countTicketsByState = (tickets: Ticket[], stateName: string): number =>
     tickets.reduce((count, ticket) => (ticket.StateName === stateName ? count + 1 : count), 0);
 
-export const getNewTickets = tickets => tickets.filter(ticket => ticket.StateName === "Nouveau");
+export const getNewTickets = (tickets: Ticket[]): Ticket[] => tickets.filter(ticket => ticket.StateName === 'Nouveau');
+
+export const getLastNewTickets = (tickets: Ticket[]): Ticket[] => tickets.filter(ticket => ticket.RequestDate === '2010-04-19T07:29:09.5800000');
